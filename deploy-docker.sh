8b3e37a2197ecfda9e400f58b373f340d4c1ac72
Jeremy <jeremy2004tw@gmail.com>
2026-08-21 14:39:28 -0500

    update workflow for cloud run with container

--- deploy-docker.sh
+++ deploy-docker.sh
@@ -1,28 +1,46 @@
 
 # Create a Repository
 gcloud artifacts repositories create gcp-cloud-run-docker \
+  --project="$PROJECT_ID" \
+  --location="$REGION" \
   --repository-format=docker \
-  --location=us-central1 \
   --description="Docker repository for gcp-cloud-run-docker"
+
+# gcloud artifacts repositories get-iam-policy gcp-cloud-run-docker \
+#   --location="$REGION" \
+#   --project="$PROJECT_ID"
+
+# gcloud artifacts repositories add-iam-policy-binding gcp-cloud-run-docker \
+#   --project="$PROJECT_ID" \
+#   --location="$REGION" \
+#   --member="serviceAccount:github-wif3@$PROJECT_ID.iam.gserviceaccount.com" \
+#   --role="roles/artifactregistry.writer"
+
+# gcloud iam service-accounts add-iam-policy-binding "${SERVICE_ACCOUNT_EMAIL}" \
+#   --project="$PROJECT_ID" \
+#   --role="roles/iam.workloadIdentityUser" \
+#   --member="principalSet://iam.googleapis.com/projects/$(gcloud projects describe "$PROJECT_ID" --format='value(projectNumber)')/locations/global/workloadIdentityPools/<POOL_ID>/attribute.repository/<OWNER>/<REPO>"
+
+# gcloud projects add-iam-policy-binding "$PROJECT_ID" \
+#   --member="serviceAccount:${SERVICE_ACCOUNT_EMAIL}" \
+#   --role="roles/run.developer"
 
 # Build and Tag the Image
 # Configure Docker authentication
 gcloud auth configure-docker us-central1-docker.pkg.dev
 
 # Build the image with a production-ready tag
-docker build -t us-central1-docker.pkg.dev/YOUR_PROJECT_ID/gcp-cloud-run-docker/my-app:1.0.0 .
+docker build -t us-central1-docker.pkg.dev/$PROJECT_ID/gcp-cloud-run-docker/gcp-cloud-run-docker:1.0.0 .
 
 # Push the Image
-docker push us-central1-docker.pkg.dev/YOUR_PROJECT_ID/gcp-cloud-run-docker/my-app:1.0.0
+docker push us-central1-docker.pkg.dev/$PROJECT_ID/gcp-cloud-run-docker/gcp-cloud-run-docker:1.0.0
 
 # Deploy to Google Cloud Run
-gcloud run deploy my-app-service \
-  --image=us-central1-docker.pkg.dev/YOUR_PROJECT_ID/gcp-cloud-run-docker/my-app:1.0.0 \
+gcloud run deploy gcp-cloud-run-docker \
+  --image=us-central1-docker.pkg.dev/$PROJECT_ID/gcp-cloud-run-docker/gcp-cloud-run-docker:1.0.0 \
   --platform=managed \
   --region=us-central1 \
   --allow-unauthenticated
-
-
 
 
 
